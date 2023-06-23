ea346e08c5f136a81a3b106394256985f06a50d3
4ebula <4ebula@gmail.com>
2023-06-24 00:41:03 +0400

    implement file operations

--- src/file-handler.js
+++ src/file-handler.js
@@ -1,9 +1,120 @@
+import { createReadStream } from 'node:fs';
 import fsPromises from 'node:fs/promises';
 import { join } from 'node:path';
 import process from 'node:process';
+import { MESSAGES } from './messages.js';
 
 export async function isFile(fileName) {
   const path = join(process.cwd(), fileName);
-  const stat =  await fsPromises.stat(path);
+  const stat = await fsPromises.stat(path);
   return stat.isFile();
+}
+
+export async function openFile(fileName) {
+  const res = await readFile(fileName);
+  console.log(res);
+}
+
+export async function readFile(fileName) {
+  const filePath = join(process.cwd(), fileName);
+
+  const rs = createReadStream(filePath);
+
+  return new Promise(res => {
+    let data = '';
+    rs.on('data', chunk => data += chunk);
+
+    rs.on('error', () => {
+      res(MESSAGES.operationFailed);
+    });
+
+    rs.on('end', () => {
+      res(data);
+    });
+  });
+}
+
+export async function createFile(fileName) {
+  const filePath = join(process.cwd(), fileName);
+  try {
+    await fsPromises.writeFile(fileName, '', { flags: 'wx+' });
+  } catch {
+    console.log(MESSAGES.operationFailed);
+  }
+}
+
+export async function renameFile(names) {
+  const splitedNames = splitNames(names);
+
+  if (!splitedNames) {
+    console.log(MESSAGES.operationFailed);
+    return;
+  }
+
+  const [oldName, newName] = splitedNames;
+
+  try {
+    await fsPromises.rename(oldName, newName);
+  } catch {
+    console.log(MESSAGES.operationFailed);
+  }
+}
+
+export async function deleteFile(fileName) {
+  try {
+    await fsPromises.unlink(fileName);
+  } catch {
+    console.log(MESSAGES.operationFailed);
+  }
+}
+
+export async function copyFile(names, deleteOld = false) {
+  const splitedNames = splitNames(names);
+
+  if (!splitedNames) {
+    console.log(MESSAGES.operationFailed);
+    return;
+  }
+
+  const [oldName, newName] = splitedNames;
+
+  try {
+    await fsPromises.copyFile(oldName, newName);
+  } catch {
+    console.log(MESSAGES.operationFailed);
+  }
+
+  if (deleteOld) {
+    deleteFile(oldName);
+  }
+}
+
+function splitNames(names) {
+  const spaces = names.match(/\s/g);
+  if (!spaces) {
+    return;
+  }
+
+  let oldName, newName;
+  let spaceIndex;
+  let isFirstWithQuotes = false;
+  const namesArr = names.split('');
+
+  if (names.at(0) === '"') {
+    oldName = names.match(/".*?"/)[0].replace(/"/g, '');
+    spaceIndex = oldName.length + 2;
+    isFirstWithQuotes = true;
+  } else {
+    oldName = names.split(' ')[0];
+    spaceIndex = oldName.length;
+  }
+
+  if (names.at(spaceIndex + 1) === '"') {
+    newName = isFirstWithQuotes ? names.match(/".*?"/g)[1] : names.match(/".*?"/)[0];
+    newName = newName.replace(/"/g, '');
+  } else {
+    newName = names.substring(spaceIndex + 1);
+  }
+
+  return [oldName, newName];
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import { greetUser, farewellUser } from './user.js';
 import { handleOS } from './os-handler.js';
 import { changeDirectory, goHome, goUp, listDir } from './navigation.js';
 import { MESSAGES } from './messages.js';
+import {
+  openFile,
+  createFile,
+  renameFile,
+  deleteFile,
+  copyFile,
+} from './file-handler.js';
 
 function main() {
   const rl = createInterface({
@@ -21,7 +28,8 @@ function main() {
   greetUser();
   goHome();
 
-  rl.on('line', async (line) => {
+  rl.on('line', async (input) => {
+    const line = input.trim();
     switch (true) {
       case /^\.exit$/.test(line): process.exit();
         break;
@@ -32,6 +40,18 @@ function main() {
       case /^cd\s/.test(line): changeDirectory(line);
         break;
       case /^ls$/.test(line): await listDir();
+        break;
+      case /^cat\s/.test(line): await openFile(line.substring(4));
+        break;
+      case /^add\s/.test(line): await createFile(line.substring(4));
+        break;
+      case /^rn\s/.test(line): await renameFile(line.substring(3));
+        break;
+      case /^rm\s/.test(line): await deleteFile(line.substring(3));
+        break;
+      case /^cp\s/.test(line): await copyFile(line.substring(3));
+        break;
+      case /^mv\s/.test(line): await copyFile(line.substring(3), true);
         break;
       default: console.log(MESSAGES.invalidCommand);
     }
